0443449bbc97bb7c695a0d4a8bf2def7cf28bdc1
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-02-29 11:45:38 -0600

    commit 2/29/16

---
+++ High_Low/high_low.php
@@ -0,0 +1,10 @@
+<?php
+  $randNum = rand(1,100);
+  $turns = 0;
+  $userGuess = "";
+
+  fwrite(STDOUT, "Guess a number from 1 to 100\nYour goal is to match the random number\nKeep following the prompts until you win!");
+  do {
+    if ()
+  }
+?>
