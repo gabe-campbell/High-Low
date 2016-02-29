93d7b316281b60ca5c75ea0b8e09438f838cf78e
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-02-29 14:43:40 -0600

    commit 3 2/29/16

--- High_Low/high_low.php
+++ High_Low/high_low.php
@@ -1,10 +1,43 @@
 <?php
-  $randNum = rand(1,100);
-  $turns = 0;
-  $userGuess = "";
+  
+  echo("\$argv values: ");
+  var_dump($argv);
+  echo("\$argc values: ");
+  var_dump($argc);
 
-  fwrite(STDOUT, "Guess a number from 1 to 100\nYour goal is to match the random number\nKeep following the prompts until you win!");
   do {
-    if ()
-  }
+  $randNum = mt_rand($argv[1],$argv[2]);
+  $turns = 0;
+  $userNum = "";
+  $play = 1;
+      fwrite(STDOUT, "\nGuess a number from $argv[1] to $argv[2]\n\nYour goal is to match the random number\n\nKeep following the prompts until you win!\n\n");
+      fwrite(STDOUT, "Enter a number $argv[1]-$argv[2]\n");
+      $userNum = fgets(STDIN);
+      
+      do {
+        if ($userNum < 1 || $userNum > 100 || $userNum == "") {
+          fwrite(STDOUT, "Please enter a valid number.\n");
+          $userNum = fgets(STDIN);
+        }
+      } while ($userNum < 1 || $userNum > 100 || $userNum == "");
+      
+      do {
+        if ($userNum < $randNum) {
+          fwrite(STDOUT, "\nGuess Higher!\n");
+          fwrite(STDOUT, "Enter Another number $argv[1]-$argv[2]\n");
+          $userNum = fgets(STDIN);
+          $turns++;
+        } else if($userNum > $randNum) {
+          fwrite(STDOUT, "\nGuess Lower!\n");
+          fwrite(STDOUT, "Enter Another number $argv[1]-$argv[2]\n");
+          $userNum = fgets(STDIN);
+          $turns++;
+        }
+      } while ($userNum != $randNum);
+
+      fwrite(STDOUT, "\nGood Guess! You Win!\n");
+      fwrite(STDOUT, "It took you $turns turns.\n\n");
+      fwrite(STDOUT, "\nHit Any Key to play again.\n");
+      fgets(STDIN);
+  } while ($play != 0);
 ?>
